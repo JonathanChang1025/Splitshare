6ad3a023acb70e4dc0a508c3e35e9ce6a3cab219
theJoonBug <jonathanchang1025@outlook.com>
2020-08-29 23:29:16 -0400

    Added Register Page smooth animation transition still not functionality

--- app/src/main/java/com/thejoonbug/splitshare/MainActivity.java
+++ app/src/main/java/com/thejoonbug/splitshare/MainActivity.java
@@ -2,15 +2,20 @@ package com.thejoonbug.splitshare;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActivityOptions;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Pair;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.google.android.material.textfield.TextInputLayout;
 
 public class MainActivity extends AppCompatActivity {
 
+    ImageView iv_logo;
     TextInputLayout til_username;
     TextInputLayout til_password;
     Button btn_login;
@@ -22,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         getSupportActionBar().hide();
 
+        iv_logo = (ImageView)findViewById(R.id.logo);
         til_username = (TextInputLayout)findViewById(R.id.input_username);
         til_password = (TextInputLayout)findViewById(R.id.input_password);
         btn_login = (Button)findViewById(R.id.button_login);
@@ -40,6 +46,15 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void btn_create_clicked(View v) {
-        Toast.makeText(this, "create account clicked", Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(MainActivity.this, CreateAccountActivity.class);
+
+        Pair[] pairs = new Pair[4];
+        pairs[0] = new Pair<View, String>(iv_logo, "logoTransition");
+        pairs[1] = new Pair<View, String>(til_username, "usernameTransition");
+        pairs[2] = new Pair<View, String>(til_password, "passwordTransition");
+        pairs[3] = new Pair<View, String>(btn_login, "buttonTransition");
+
+        ActivityOptions activityOptions = ActivityOptions.makeSceneTransitionAnimation(this, pairs);
+        startActivity(intent, activityOptions.toBundle());
     }
 }
