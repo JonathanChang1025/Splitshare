9ae45cc73d4fc7b8cd842a46ad2a670f3f32b6f3
theJoonBug <jonathanchang1025@outlook.com>
2020-09-02 22:39:12 -0400

    Revised Register Page cleaned up transitions

--- app/src/main/java/com/thejoonbug/splitshare/CreateAccountActivity.java
+++ app/src/main/java/com/thejoonbug/splitshare/CreateAccountActivity.java
@@ -2,9 +2,32 @@ package com.thejoonbug.splitshare;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActivityOptions;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Pair;
+import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputLayout;
+
+import org.w3c.dom.Text;
 
 public class CreateAccountActivity extends AppCompatActivity {
+
+    ImageView iv_logo;
+    TextInputLayout til_username;
+    TextInputLayout til_password;
+    TextInputLayout til_email;
+    TextInputLayout til_phonenumber;
+    AutoCompleteTextView actv_currency;
+    Button btn_register;
+    Button btn_signin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,6 +35,54 @@ public class CreateAccountActivity extends AppCompatActivity {
         setContentView(R.layout.activity_create_account);
         getSupportActionBar().hide();
 
+        iv_logo = (ImageView)findViewById(R.id.logo);
+        til_username = (TextInputLayout)findViewById(R.id.input_username);
+        til_password = (TextInputLayout)findViewById(R.id.input_password);
+        til_email = (TextInputLayout)findViewById(R.id.input_email);
+        til_phonenumber = (TextInputLayout)findViewById(R.id.input_phonenumber);
+        actv_currency = (AutoCompleteTextView)findViewById(R.id.actv_currency);
+        btn_register = (Button)findViewById(R.id.button_register);
+        btn_signin = (Button)findViewById(R.id.button_signin);
 
+        AlphaAnimation animation = new AlphaAnimation(0, 1);
+        animation.setDuration(750);
+        til_email.startAnimation(animation);
+        til_phonenumber.startAnimation(animation);
+
+        setCurrencyAdapter();
+
+    }
+
+    private void setCurrencyAdapter() {
+        String[] CURRENCY = new String[] {"CAD", "USD", "EUR", "NTD"};
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, R.layout.dropdown_menu_popup_item, CURRENCY);
+        actv_currency.setAdapter(adapter);
+        actv_currency.setHint(CURRENCY[0]);
+        actv_currency.setHintTextColor(getResources().getColor(R.color.colorLightGray));
+
+
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        btn_signin_clicked(null);
+    }
+
+    public void btn_signin_clicked(View v) {
+        //super.onBackPressed();
+        Intent intent = new Intent(CreateAccountActivity.this, MainActivity.class);
+
+        Pair[] pairss = new Pair[5];
+        pairss[0] = new Pair<View, String>(iv_logo, "logoTransition");
+        pairss[1] = new Pair<View, String>(til_username, "usernameTransition");
+        pairss[2] = new Pair<View, String>(til_password, "passwordTransition");
+        pairss[3] = new Pair<View, String>(btn_register, "submitTransition");
+        pairss[4] = new Pair<View, String>(btn_signin, "reginTransition");
+
+        ActivityOptions activityOptions = ActivityOptions.makeSceneTransitionAnimation(this, pairss);
+        //ActivityOptions activityOptions = ActivityOptions.makeSceneTransitionAnimation(this, iv_logo, "logoTransition");
+        startActivity(intent, activityOptions.toBundle());
     }
 }
--- app/src/main/java/com/thejoonbug/splitshare/MainActivity.java
+++ app/src/main/java/com/thejoonbug/splitshare/MainActivity.java
@@ -1,10 +1,14 @@
 package com.thejoonbug.splitshare;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
 import android.app.ActivityOptions;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.util.Pair;
 import android.view.View;
 import android.widget.Button;
@@ -13,14 +17,29 @@ import android.widget.Toast;
 
 import com.google.android.material.textfield.TextInputLayout;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class MainActivity extends AppCompatActivity {
 
-    ImageView iv_logo;
-    TextInputLayout til_username;
-    TextInputLayout til_password;
-    Button btn_login;
-    Button btn_create;
-    
+    final int MINIMUM_LOGIN_LENGTH = 6;
+
+    private ImageView iv_logo;
+    private TextInputLayout til_username;
+    private TextInputLayout til_password;
+    private Button btn_login;
+    private Button btn_create;
+
+    private static String ip = "b6ggdiwfbdh7mjuypfve-mysql.services.clever-cloud.com";
+    private static String port = "3306";
+    //private static String classes = "com.mysql.jdbc.Driver";
+    private static String classes = "net.sourceforge.jtds.jdbc.Driver";
+    private static String database = "b6ggdiwfbdh7mjuypfve";
+    private static String username = "ug2tmdiev70mdel3";
+    private static String password = "dfw4II0iWROw6elCdnw5";
+    private static String url = "jdbc:jtds:mysql://"+ip+":"+port+"/"+database;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,7 +51,7 @@ public class MainActivity extends AppCompatActivity {
         til_password = (TextInputLayout)findViewById(R.id.input_password);
         btn_login = (Button)findViewById(R.id.button_login);
         btn_create = (Button)findViewById(R.id.button_create);
-        
+
     }
 
     @Override
@@ -40,19 +59,35 @@ public class MainActivity extends AppCompatActivity {
         til_username.clearFocus();
         til_password.clearFocus();
     }
+
+    private boolean validate_string_length(String input) {
+        if (input.length() >= MINIMUM_LOGIN_LENGTH) {
+            return true;
+        } else {
+            Toast.makeText(this, String.format("Username/Password should be at least %2d characters", MINIMUM_LOGIN_LENGTH), Toast.LENGTH_SHORT).show();
+            return false;
+        }
+
+    }
     
     public void btn_login_clicked(View v) {
-        Toast.makeText(this, "login button clicked", Toast.LENGTH_SHORT).show();
+        String username = til_username.getEditText().getText().toString();
+        String password = til_password.getEditText().getText().toString();
+
+        if (validate_string_length(username) && validate_string_length(password)) {
+            Toast.makeText(this, username + " " + password, Toast.LENGTH_SHORT).show();
+        }
     }
 
     public void btn_create_clicked(View v) {
         Intent intent = new Intent(MainActivity.this, CreateAccountActivity.class);
 
-        Pair[] pairs = new Pair[4];
+        Pair[] pairs = new Pair[5];
         pairs[0] = new Pair<View, String>(iv_logo, "logoTransition");
         pairs[1] = new Pair<View, String>(til_username, "usernameTransition");
         pairs[2] = new Pair<View, String>(til_password, "passwordTransition");
-        pairs[3] = new Pair<View, String>(btn_login, "buttonTransition");
+        pairs[3] = new Pair<View, String>(btn_login, "submitTransition");
+        pairs[4] = new Pair<View, String>(btn_create, "reginTransition");
 
         ActivityOptions activityOptions = ActivityOptions.makeSceneTransitionAnimation(this, pairs);
         startActivity(intent, activityOptions.toBundle());
